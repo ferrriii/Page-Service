02d0417c735d2a50e4a01a3431093d4a2dba6206
ferrriii <57658129+ferrriii@users.noreply.github.com>
2022-10-16 12:08:11 +0330

    Remove dotenv

--- src/index.js
+++ src/index.js
@@ -1,20 +1,13 @@
 // @flow
-import * as dotenv from 'dotenv'
 import cors from 'cors'
 import express from 'express'
 import bodyParser from 'body-parser'
 import v1Routes from './routes/v1Routes.js'
 
-// load configs from .env files
-dotenv.config()
-
 const app = express()
 const port = process.env.PORT || 8080
 
 app.use(cors())
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse requests of content-type - application/json
 app.use(bodyParser.json())
@@ -27,4 +20,5 @@ const server = app.listen(port, () => {
   console.log(`Service is listening on port ${port}`)
 })
 
+// export for testing purpose
 module.exports = server
